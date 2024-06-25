4d6fb1c7a77339bda1516a0f6800e4d489dc45ea
Jessica Martin <lc22-0571@lclark.edu>
2024-06-25 23:09:17 +0000

    Inital Commit

---
+++ simulate-plot-data.R
@@ -0,0 +1,11 @@
+# simulate and plot data
+# Jessica Martin
+# LC22-0571@lclark.edu
+# 2024-06-25
+
+# Simulate predictor variable
+x <- rnorm(n = 100)
+# Simulate response variable with some noise
+y <- 2 * x + rnorm(n = 100, sd = 0.2)
+# Plot the data
+plot(x = x, y = y)
